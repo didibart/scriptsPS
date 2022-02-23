8ec6a3df08211a5ce657003577e571f00ca9140a
didibart <didibart@gmail.com>
2022-02-23 10:31:31 -0300

    agrega lineas a txt al comienzo

---
+++ agregalineaatxt.ps1
@@ -0,0 +1,16 @@
+﻿
+$encabezado = '*nota_'
+$ruta = 'C:\RWS\SaludInvestiga2022\ene22'
+$rutatxt = 'C:\RWS\SaludInvestiga2022\ene22\pepe.txt'
+$files = Get-ChildItem $ruta -Filter *.txt
+#sacar los segui leyendo
+for ($i=0; $i -lt $files.Count; $i++) {
+    $encabezado = $encabezado + $i
+    $encabezado | Set-Content $rutatxt
+    $contenido = Get-Content $files[$i].FullName 
+    Add-Content $rutatxt -Value $contenido
+    $rutaarchivo = $ruta + '\' + $files[$i]
+    Remove-item $rutaarchivo
+    Rename-Item $rutatxt -NewName $rutaarchivo
+    $encabezado = '*nota_'
+}
