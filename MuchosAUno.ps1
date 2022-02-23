9b24652e0feaf50b1295252fb4b74a4870d5babf
didibart <didibart@gmail.com>
2022-02-23 10:59:37 -0300

    de varios archivos a uno solo

---
+++ MuchosAUno.ps1
@@ -0,0 +1,2 @@
+﻿$MySourcePath = "C:\RWS\Saludinvestiga2022\ene22"
+Get-ChildItem $MySourcePath -Recurse -Include "*.txt" | get-content |  Out-File -Encoding default -Append C:\RWS\Saludinvestiga2022\ene22\salida\todos.txt
