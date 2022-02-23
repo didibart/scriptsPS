e27c067b92a31d7ffea42825ff396ace497dc338
didibart <didibart@gmail.com>
2022-02-23 11:12:46 -0300

    escribe en un archivo de texto

---
+++ EnUnArchivoDeTextoAgregaTexto.ps1
@@ -0,0 +1,20 @@
+﻿$fileName = "C:\RWS\UNDEC\entrevistas\salida\todos.txt"
+$Patern = "<patern>" # the 2 lines will be added just after this pattern 
+$FileOriginal = Get-Content $fileName
+
+<# create empty Array and use it as a modified file... #>
+
+$FileModified = @() 
+
+Foreach ($Line in $FileOriginal)
+{    
+    $FileModified += $Line
+
+    if ($Line -match $patern) 
+    {
+        #Add Lines after the selected pattern 
+        $FileModified += 'add text'
+        $FileModified += 'add second line text'
+    } 
+}
+Set-Content $fileName $FileModified
