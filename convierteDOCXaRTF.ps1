ff1e72f6012fe77ca3e138aa07546ab994369359
didibart <didibart@gmail.com>
2022-02-23 11:04:33 -0300

    de doc a rtf

---
+++ convierteDOCXaRTF.ps1
@@ -0,0 +1,12 @@
+﻿$files = Get-ChildItem "C:\RWS\UNLaM\entrevistas2021\salida\*.txt"
+$ruta="C:\RWS\UNLaM\entrevistas2021\salida\"
+for ($i=0; $i -lt $files.Count; $i++)
+{
+  $nomarch=$files[$i].BaseName
+  $name = -join($nomarch, ".rtf")
+  $nameCO = -join($ruta,$name)
+  #echo $files[$i].FullName
+  #echo $nameCO
+  #pandoc -f docx $files[$i].FullName -t rtf -o $nameCO
+  pandoc -t textfile -s $files[$i].FullName -t rtf -o $nameCO
+}
